6451bd3d49b25f7a9fa41df1b0382b2676f57176
vidula-27 <65344162+vidula-27@users.noreply.github.com>
2026-09-09 00:19:50 +0530

    Update Business_Queries.sql

--- Business_Queries.sql
+++ Business_Queries.sql
@@ -1,1 +1,8 @@
-
+--1. What are the Top 10 products by net revenue?
+Select top 10 p.Product_Name,
+       cast(sum(s.Quantity * s.Unit_Price * (1 - s.Discount )) as decimal(10 ,2)) as 'Revenue'
+from SalesInventoryMIS.dbo.Sales s
+left outer join SalesInventoryMIS.dbo.Stores st on s.Store_ID = st.Store_ID
+left outer join SalesInventoryMIS.dbo.Products p on p.Product_ID = s.Product_ID
+group by p.Product_Name
+order by sum(s.Quantity * s.Unit_Price * (1 - s.Discount )) desc
